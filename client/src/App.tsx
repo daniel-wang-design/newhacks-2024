1c661304fc18401773544589dabfe423d99be88f
mttheus <137439401+mttheus@users.noreply.github.com>
2024-10-26 21:04:39 -0400

    numpad and voicemail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Incoming from "./incoming/incoming";
+import Voicemail from "./voicemail/voicemail";
+import Numpad from "./numpad/numpad";
+import RealTimeComponent from "./websockets/websockets";
+import Incall from "./incall/incall";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Routes>
+            <Route path="/incoming" element={<Incoming></Incoming>}></Route>
+            <Route path="/voicemail" element={<Voicemail></Voicemail>}></Route>
+            <Route path="/" element={<Numpad></Numpad>}></Route>
+            <Route
+              path="/real"
+              element={<RealTimeComponent></RealTimeComponent>}
+            ></Route>
+            <Route path="/call" element={<Incall></Incall>}></Route>
+          </Routes>
+        </Router>
       </header>
     </div>
   );
---
+++ client/src/numpad/numpad.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './numpad.css';
+
+function Numpad() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+<<<<<<< HEAD:client/src/numpad/App.tsx
+          Edit <code>src/App.tsx</code> and save to testing.
+=======
+          Edit <code>src/App.tsx</code> and save to numpad.
+>>>>>>> 1c1e9c744b4fcccc44be83b7bb62d36a00848986:client/src/numpad/numpad.tsx
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Numpad;
